c2545a27424900f79e169c427699e6bcde3a3e89
Kadriye Doğan <kadriye.dogan@iyzico.com>
2016-04-12 16:35:28 +0300

    fixes #28 3ds preAuth implemented

---
+++ iyzipay-dotnet/Model/ThreeDSInitializePreAuth.cs
@@ -0,0 +1,23 @@
+﻿using Iyzipay.Request;
+using System;
+using Newtonsoft.Json;
+
+namespace Iyzipay.Model
+{
+    public class ThreeDSInitializePreAuth : IyzipayResource
+    {
+        [JsonProperty(PropertyName = "threeDSHtmlContent")]
+        public String HtmlContent { get; set; }
+
+        public static ThreeDSInitializePreAuth Create(CreateThreeDSInitializeRequest request, Options options)
+        {
+            ThreeDSInitializePreAuth response = RestHttpClient.Create().Post<ThreeDSInitializePreAuth>(options.BaseUrl + "/payment/iyzipos/initialize3ds/preauth/ecom", GetHttpHeaders(request, options), request);
+
+            if (response != null)
+            {
+                response.HtmlContent = DigestHelper.decodeString(response.HtmlContent);
+            }
+            return response;
+        }
+    }
+}

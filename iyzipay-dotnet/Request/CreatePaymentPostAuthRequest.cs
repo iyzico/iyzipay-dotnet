f2aef72ea9fa03302445400b4059887c026b2575
Kurtuluş Şahin <kurtulussahin@users.noreply.github.com>
2016-04-24 18:31:12 +0300

    partial post auth implemented
    partial post auth implemented (issue #31)

--- iyzipay-dotnet/Request/CreatePaymentPostAuthRequest.cs
+++ iyzipay-dotnet/Request/CreatePaymentPostAuthRequest.cs
@@ -5,14 +5,16 @@ namespace Iyzipay.Request
     public class CreatePaymentPostAuthRequest : BaseRequest
     {
         public String PaymentId { get; set; }
+        public String PaidPrice { get; set; }
         public String Ip { get; set; }
 
         public override String ToPKIRequestString()
         {
             return ToStringRequestBuilder.NewInstance()
                 .AppendSuper(base.ToPKIRequestString())
-                .Append("paymentId", PaymentId)
+                .Append("paymentId", PaymentId)                
                 .Append("ip", Ip)
+                .Append("paidPrice", PaidPrice)
                 .GetRequestString();
         }
     }

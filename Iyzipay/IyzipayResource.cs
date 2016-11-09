1bdb5374fe3a2a9bb79685e6d90026fa8a2c64de
kurtulussahin <kurtulussahin@gmail.com>
2016-11-09 15:57:39 +0200

    version added

--- Iyzipay/IyzipayResource.cs
+++ Iyzipay/IyzipayResource.cs
@@ -7,6 +7,7 @@ namespace Iyzipay
     {
         private static readonly String AUTHORIZATION = "Authorization";
         private static readonly String RANDOM_HEADER_NAME = "x-iyzi-rnd";
+        private static readonly String CLIENT_VERSION = "x-iyzi-client-version";
         private static readonly String IYZIWS_HEADER_NAME = "IYZWS ";
         private static readonly String COLON = ":";
 
@@ -28,6 +29,7 @@ namespace Iyzipay
             WebHeaderCollection headers = new WebHeaderCollection();
             headers.Add("Accept", "application/json");
             headers.Add(RANDOM_HEADER_NAME, randomString);
+            headers.Add(CLIENT_VERSION, "iyzipay-dotnet-2.1.6");
             headers.Add(AUTHORIZATION, PrepareAuthorizationString(request, randomString, options));
             return headers;
         }

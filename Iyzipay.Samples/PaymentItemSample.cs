fd228ea7db132db9e8f34bda9b7e95ac1c3bab23
ccizer <can.cizer@gmail.com>
2018-04-04 16:13:53 +0300

    Update payment item feature is added.

---
+++ Iyzipay.Samples/PaymentItemSample.cs
@@ -0,0 +1,32 @@
+﻿using Iyzipay.Request;
+using Iyzipay.Model;
+using NUnit.Framework;
+
+namespace Iyzipay.Samples
+{
+    public class PaymentItemSample : Sample
+    {
+        [Test]
+        public void Should_Update_Payment_Item()
+        {
+            UpdatePaymentItemRequest request = new UpdatePaymentItemRequest();
+            request.Locale = Locale.TR.ToString();
+            request.ConversationId = "123456789";
+            request.SubMerchantKey = "subMerchantKey";
+            request.PaymentTransactionId = "transactionId";
+            request.SubMerchantPrice = "price";
+
+            PaymentItem paymentItem = PaymentItem.Update(request, options);
+
+            PrintResponse<PaymentItem>(paymentItem);
+
+            Assert.AreEqual(Status.SUCCESS.ToString(), paymentItem.Status);
+            Assert.AreEqual(Locale.TR.ToString(), paymentItem.Locale);
+            Assert.AreEqual("123456789", paymentItem.ConversationId);
+            Assert.IsNotNull(paymentItem.SystemTime);
+            Assert.IsNull(paymentItem.ErrorCode);
+            Assert.IsNull(paymentItem.ErrorMessage);
+            Assert.IsNull(paymentItem.ErrorGroup);
+        }
+    }
+}

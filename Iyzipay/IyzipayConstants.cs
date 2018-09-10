2cfd3d95ff9fe341d74cad5dd0e0ca76f52ef6d5
= <=>
2018-09-10 16:06:44 +0300

    Client Version is updated to 2.1.15

--- Iyzipay/IyzipayConstants.cs
+++ Iyzipay/IyzipayConstants.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Iyzipay
 {
     public class IyzipayConstants
     {
-        public static readonly String CLIENT_VERSION = "iyzipay-dotnet-2.1.14";
+        public static readonly String CLIENT_VERSION = "iyzipay-dotnet-2.1.15";
     }
 }

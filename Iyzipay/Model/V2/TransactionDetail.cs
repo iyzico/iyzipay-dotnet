3e96c234b43936fc6c9cf4b3c6529ab7d5f13608
can3ry <51880555+can3ry@users.noreply.github.com>
2019-06-17 19:52:18 +0300

    Remove pacco and add transaction details (#90)
    * pecco removed and transaction detail endpoint added
    
    * spaces were reomved

---
+++ Iyzipay/Model/V2/TransactionDetail.cs
@@ -0,0 +1,16 @@
+﻿using Iyzipay.Request.V2;
+using System;
+
+namespace Iyzipay.Model.V2
+{
+    public class TransactionDetail : TransactionReportResource
+    {
+        public static TransactionDetail Retrieve(RetrieveTransactionDetailRequest request, Options options)
+        {
+            String url = options.BaseUrl
+                + "/v2/reporting/payment/details?paymentConversationId="
+                + request.PaymentConversationId;
+            return RestHttpClientV2.Create().Get<TransactionDetail>(url, GetHttpHeadersWithUrlParams(request, url, options));
+        }
+    }
+}

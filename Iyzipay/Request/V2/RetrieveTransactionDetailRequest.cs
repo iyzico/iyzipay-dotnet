1e8f77f4a60bf3c62f39213835983d003a42b99b
Caner Yazıcı <contact@iyzico.com>
2019-06-17 12:25:05 +0300

    pecco removed and transaction detail endpoint added

---
+++ Iyzipay/Request/V2/RetrieveTransactionDetailRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Iyzipay.Request.V2
+{
+    public class RetrieveTransactionDetailRequest : BaseRequestV2
+    {
+        public String PaymentConversationId { get; set; }
+    }
+}

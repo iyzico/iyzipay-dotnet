8ff98062ad391119824a7e548027bc3014dd01b6
okan.bisgin <okan.bisgin@iyzico.com>
2020-01-15 23:13:07 +0300

    added retrieve iyziup form request builder

---
+++ Iyzipay/Request/RetrieveIyziupFormRequest.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Iyzipay.Request
+{
+    public class RetrieveIyziupFormRequest : BaseRequest
+    {
+        public String Token { set; get; }
+
+        public override String ToPKIRequestString()
+        {
+            return ToStringRequestBuilder.NewInstance()
+                .AppendSuper(base.ToPKIRequestString())
+                .Append("token", Token)
+                .GetRequestString();
+        } 
+    }
+}

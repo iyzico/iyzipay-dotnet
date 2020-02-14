a474e15017fca766ae75b1604b5ebff0bae714b8
Halit Soydemir <halit.soydemir@iyzico.com>
2020-02-14 14:38:15 +0300

    commercial parameter on binnumber
    - added commercial parameter on bin number class

--- Iyzipay.Tests/Functional/BinNumberTest.cs
+++ Iyzipay.Tests/Functional/BinNumberTest.cs
@@ -30,7 +30,37 @@ namespace Iyzipay.Tests.Functional
             Assert.AreEqual("MASTER_CARD", binNumber.CardAssociation);
             Assert.AreEqual("Bonus", binNumber.CardFamily);
             Assert.AreEqual("Garanti Bankası", binNumber.BankName);
+            Assert.AreEqual(0, binNumber.Commercial);
             Assert.AreEqual(62, binNumber.BankCode);
         }
+        
+        [Test]
+        public void Should_Retrieve_Bin_With_Commercial_One()
+        {
+            RetrieveBinNumberRequest request = RetrieveBinNumberRequestBuilder.Create()
+                .BinNumber("552659")
+                .Build();
+
+            BinNumber binNumber = BinNumber.Retrieve(request, _options);
+
+            PrintResponse(request);
+
+            Assert.AreEqual(Status.SUCCESS.ToString(), binNumber.Status);
+            Assert.AreEqual(Locale.TR.ToString(), binNumber.Locale);
+            Assert.AreEqual("123456789", binNumber.ConversationId);
+            Assert.NotNull(binNumber.SystemTime);
+            Assert.Null(binNumber.ErrorCode);
+            Assert.Null(binNumber.ErrorMessage);
+            Assert.Null(binNumber.ErrorGroup);
+            Assert.AreEqual("552659", binNumber.Bin);
+            Assert.AreEqual("CREDIT_CARD", binNumber.CardType);
+            Assert.AreEqual("MASTER_CARD", binNumber.CardAssociation);
+            Assert.AreEqual("World", binNumber.CardFamily);
+            Assert.AreEqual("Yapı Kredi Bankası", binNumber.BankName);
+            Assert.AreEqual(1, binNumber.Commercial);
+            Assert.AreEqual(67, binNumber.BankCode);
+        }
     }
+    
+    
 }

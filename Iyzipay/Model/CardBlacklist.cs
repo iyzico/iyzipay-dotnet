bda704e7b59ac7bb0e2f86d1be756309198b6482
furkanbatuhan <batuhan.ilhan@iyzico.com>
2022-09-06 11:56:40 +0300

    Adds Card Blacklist Feature

---
+++ Iyzipay/Model/CardBlacklist.cs
@@ -0,0 +1,27 @@
+﻿using Iyzipay.Request;
+using System;
+
+namespace Iyzipay.Model
+{
+    public class CardBlacklist : IyzipayResource
+    {
+        public String CardUserKey { get; set; }
+        public String CardToken { get; set; }
+        public String CardNumber { get; set; }
+        public Boolean Blacklisted { get; set; }
+        public static CardBlacklist Create(CreateCardBlacklistRequest request, Options options)
+        {
+            return RestHttpClient.Create().Post<CardBlacklist>(options.BaseUrl + "/cardstorage/blacklist/card", GetHttpHeaders(request, options), request);
+        }
+
+        public static CardBlacklist Update(UpdateCardBlacklistRequest request, Options options)
+        {
+            return RestHttpClient.Create().Post<CardBlacklist>(options.BaseUrl + "/cardstorage/blacklist/card/inactive", GetHttpHeaders(request, options), request);
+        }
+
+        public static CardBlacklist Retrieve(RetrieveCardBlacklistRequest request, Options options)
+        {
+            return RestHttpClient.Create().Post<CardBlacklist>(options.BaseUrl + "/cardstorage/blacklist/cards", GetHttpHeaders(request, options), request);
+        }
+    }
+}
---
+++ Iyzipay/Request/RetrieveCardBlacklistRequest.cs
@@ -0,0 +1,18 @@
+﻿using Iyzipay.Model;
+using System;
+
+namespace Iyzipay.Request
+{
+    public class RetrieveCardBlacklistRequest : BaseRequest
+    {
+        public String CardNumber { get; set; }
+
+        public override String ToPKIRequestString()
+        {
+            return ToStringRequestBuilder.NewInstance()
+                .AppendSuper(base.ToPKIRequestString())
+                .Append("cardNumber", CardNumber)
+                .GetRequestString();
+        }
+    }
+}

6f0acc4dd8af6c9555373549051247e0f57d7d9e
Erdem Demir <35841628+erdemdmr@users.noreply.github.com>
2022-12-08 15:17:11 +0300

    Adds checkoutform loyalty inquire (#124)

---
+++ Iyzipay/Model/LoyaltyInquiry.cs
@@ -0,0 +1,19 @@
+﻿using Iyzipay.Request;
+using System;
+
+namespace Iyzipay.Model
+{
+    public class LoyaltyInquiry : IyzipayResource
+    {
+        public string Points { get; set; }
+        public string Amount { get; set; }
+        public string CardBank { get; set; }
+        public string CardFamily { get; set; }
+        public string Currency { get; set; }
+
+        public static LoyaltyInquiry Create(LoyaltyInquiryRequest request, Options options)
+        {
+            return RestHttpClient.Create().Post<LoyaltyInquiry>(options.BaseUrl + "/payment/loyalty/inquire", GetHttpHeaders(request, options), request);
+        }
+    }
+}

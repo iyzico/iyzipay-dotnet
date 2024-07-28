623ccd5b53a5e9e286843e8f0c307625298b31cd
eeyuksel <eyup.yuksel@basefy.com>
2024-07-28 22:25:23 +0300

    validation dosyaları eklendi

--- Iyzipay.Samples/SignatureVerification.cs
+++ Iyzipay.Samples/SignatureVerification.cs
@@ -2,21 +2,38 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Iyzipay.Samples
+public class SignatureGeneration
 {
-	public class SignatureVerification
-	{
-		public static string CalculateHmacSHA256Signature(string secretKey, string[] parameters)
-		{
-			string dataToSign = string.Join(":", parameters);
-			byte[] keyBytes = Encoding.UTF8.GetBytes(secretKey);
-			byte[] dataBytes = Encoding.UTF8.GetBytes(dataToSign);
+    public static void Main()
+    {
+        // Sample hash signature calculation for the /payment/auth endpoint response signature.
+        // You can refer to https://docs.iyzico.com/v/en/advanced/response-signature-validation for other endpoints and signature parameters.
+        string secretKey = "sandbox-qaIiLIxhjMgx3LSKIVvp6j17NunHOFtD";
+        string[] parameters = {
+            "22416032",   // paymentId
+            "TRY",        // currency
+            "basketId",	  // basketId
+            "conversationId",  // conversationId
+            "10.5",       // paidPrice
+            "10.5"        // price
+        };
 
-			using (var hmac = new HMACSHA256(keyBytes))
-			{
-				byte[] hashBytes = hmac.ComputeHash(dataBytes);
-				return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-			}
-		}
-	}
+        string hashedSignature = CalculateHMACSHA256(parameters, secretKey);
+
+        Console.WriteLine("Generated Signature: " + hashedSignature);
+    }
+
+    // HashSignature
+    private static string CalculateHMACSHA256(string[] data, string key)
+    {
+        string dataToEncrypt = string.Join(":", data);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] dataBytes = Encoding.UTF8.GetBytes(dataToEncrypt);
+
+        using (var hmac = new HMACSHA256(keyBytes))
+        {
+            byte[] hmacBytes = hmac.ComputeHash(dataBytes);
+            return BitConverter.ToString(hmacBytes).Replace("-", "").ToLower();
+        }
+    }
 }

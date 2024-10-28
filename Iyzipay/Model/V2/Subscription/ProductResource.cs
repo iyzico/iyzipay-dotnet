00db589fccb693c205d04db1404e110accc43869
Cansin Senalioglu <cansin@startupcentrum.com>
2024-10-29 02:01:10 +0300

    ProductResource PricingPlans model type fix

--- Iyzipay/Model/V2/Subscription/ProductResource.cs
+++ Iyzipay/Model/V2/Subscription/ProductResource.cs
@@ -8,6 +8,6 @@ namespace Iyzipay.Model.V2.Subscription
         public long? CreatedDate { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Plan> PricingPlans { get; set; }
+        public List<PlanResource> PricingPlans { get; set; }
     }
 }

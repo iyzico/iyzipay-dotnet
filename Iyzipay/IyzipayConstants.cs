a90f42af284a3506bd181f7f42f72d592eebd2b5
iyzico-ci <iyzico-ci@iyzico.com>
2026-01-30 10:03:27 +0000

    Automatic commit by iyzico-ci v2.1.73

--- Iyzipay/IyzipayConstants.cs
+++ Iyzipay/IyzipayConstants.cs
@@ -4,6 +4,6 @@ namespace Iyzipay
 {
     public class IyzipayConstants
     {
-        public static readonly String CLIENT_VERSION = "iyzipay-dotnet-2.1.72";
+        public static readonly String CLIENT_VERSION = "iyzipay-dotnet-2.1.73";
     }
 }

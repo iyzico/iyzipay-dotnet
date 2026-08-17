a76ef425bdf15b62cc144d2a2406563a2baf4e14
Bünyamin Yaşar <33955141+byasarcse@users.noreply.github.com>
2026-08-17 14:21:10 +0300

    Update CardManagementPage BaseUrl
    Signed-off-by: Bünyamin Yaşar <33955141+byasarcse@users.noreply.github.com>

--- Iyzipay.Samples/CardManagementPageSample.cs
+++ Iyzipay.Samples/CardManagementPageSample.cs
@@ -11,7 +11,7 @@ namespace Iyzipay.Samples
         public void SetUp()
         {
             Initialize();
-            options.BaseUrl = "https://sandbox-cm.iyzipay.com";
+            options.BaseUrl = "https://sandbox-api.iyzipay.com";
         }
         
         [Test]
